19c75fc0f497aecf10201dfd4a77da5e7d1fd2f3
ummarmtp <ummarmtp@gmail.com>
2023-06-19 12:19:39 +0400

    tostring

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.get("/",function(req,res){res.render('index');});
 
 
-app.get("/id",function(req,res){res.send(buttonValue);});
+app.get("/id",function(req,res){res.send(buttonValue.toString());});
 
 var buttonValue=0;
 
