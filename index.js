362d17a3ed530999e261880d5146446a4cbe0b4b
ummarmtp <ummarmtp@gmail.com>
2023-06-19 10:51:28 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var http=require('http');
+var express=require('express');
+
+var app=express();
+var server=http.createServer(app);
+var io=require('socket.io')(server);
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+app.get("/",function(req,res){res.render('index');});
+
+var buttonValue=0;
+
+
+io.on('connection',function(socket){
+    console.log('client connected.....');
+    io.emit('clicked message',buttonValue);
+    socket.on('clicked message',function(msg){
+        buttonValue=1-buttonValue; 
+        io.emit('clicked message',buttonValue);
+        console.log('Received message from client!',msg);
+    
+    });
+
+    socket.on('disconnect' , function(){
+        console.log('cleint disconnected.....');
+    });
+});
+
+
+
+
+
+server.listen(3000);
